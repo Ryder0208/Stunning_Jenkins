aa1701fea8ab00d27ef6bc2041fa24e430e7f83f
Ryder0208 <vitthalg123@gmail.com>
2022-07-22 17:09:43 +0530

    file added

---
+++ src/main/java/com/vir/JTest.java
@@ -0,0 +1,14 @@
+package com.vir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JTest {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JTest.class, args);
+		System.out.println("***Welcome to Jtest Program..***");
+	}
+
+}
